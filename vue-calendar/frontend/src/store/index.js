73b97f71d43677359a03dc212bde9f4bb6d3bbee
takashiono2 <takashiono710@gmail.com>
2021-11-20 20:04:00 +0900

    3-2途中

--- vue-calendar/frontend/src/store/index.js
+++ vue-calendar/frontend/src/store/index.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import events from './modules/events';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules:{
+    events
+  }//modules:で使う事を決める
 });
